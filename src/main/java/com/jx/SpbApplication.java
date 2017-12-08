bc5e95f253d8613b54190376f1765b58f3513e4e
Jingxuan <Jingxuan>
2017-12-08 15:45:06 +0800

    第一次提交xuan的项目

---
+++ src/main/java/com/jx/SpbApplication.java
@@ -0,0 +1,12 @@
+package com.jx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbApplication.class, args);
+	}
+}
---
+++ src/main/java/com/jx/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.jx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by Qiju on 11/28/2017.
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping(value = "/hello")
+    public String index(){
+        return "welcome";
+    }
+}
